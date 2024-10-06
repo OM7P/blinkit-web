f4ba55c4f230618e16f07fe9b1cabdafee21de2e
omprakash-boppo <omprakash.prajapati@boppotechnologies.com>
2024-10-06 20:51:02 +0530

    working on cards

--- src/app/HeaderPage/shopingbutton.jsx
+++ src/app/HeaderPage/shopingbutton.jsx
@@ -5,14 +5,14 @@ import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaRupeeSign } from "react-icons/fa";
 
-function ShopingButton() {
+function ShopingButton({count}) {
   return (
     <div className="">
       <Button className="text-white  bg-green-700 w-[120px] h-[50px] hover:bg-green-500">
         <div className="gap-4 flex justify-around items-center">
           <FaShoppingCart className="w-[20px] h-[20px] " />
           <div className=" text-[19px] font-semibold text-start">
-            <p>5 items</p>
+            <p>{count} items</p>
             <p className="flex justify-center items-center">
               <FaRupeeSign />
               987
--- src/app/dashboard/Dashboard.jsx
+++ src/app/dashboard/Dashboard.jsx
@@ -1,38 +1,53 @@
-import React from 'react'
-import Header from '../HeaderPage/header'
-import Banner_first from '../banner_1/Banner_first'
-import Banner_Second from '../banner_2/banner_Second'
-import MultipleIteams from '../component/MultipleIteams'
-import Daily_base from '../component/Daily_base'
-import Rolling_Paper from '../component/Rolling_paper_bacco'
-import Sancks_Munchies from '../component/Snacks & Munchies'
-import Hookah from '../component/Hookah'
-import Mouth_Fresh from '../component/Mouth fresheners'
-import Cold_Drinks from '../component/Cold Drinks & Juices'
-import Candies_Gums from '../component/Candies & Gums'
-import LinkCategories from '../component/Limk_Categories'
-import Footer from '../component/Footer'
-// import Demo_Items from '../component/demo_items'
+"use client";
+
+import { React, useState,useEffect } from "react";
+import Header from "../HeaderPage/header";
+import Banner_first from "../banner_1/Banner_first";
+import Banner_Second from "../banner_2/banner_Second";
+import MultipleIteams from "../component/MultipleIteams";
+import Daily_base from "../component/Daily_base";
+import Rolling_Paper from "../component/Rolling_paper_bacco";
+import Sancks_Munchies from "../component/Snacks & Munchies";
+import Hookah from "../component/Hookah";
+import Mouth_Fresh from "../component/Mouth fresheners";
+import Cold_Drinks from "../component/Cold Drinks & Juices";
+import Candies_Gums from "../component/Candies & Gums";
+import LinkCategories from "../component/Limk_Categories";
+import Footer from "../component/Footer";
+import Loader from "../component/Loader";
+
 
 function Dashboard() {
-  return (
-    <div>
-        <Header/>
-        <Banner_first/>
-        <Banner_Second/>
-        <MultipleIteams/>
-        <Daily_base/>
-        <Rolling_Paper/>
-        <Sancks_Munchies/>
-        <Hookah/>
-        <Mouth_Fresh/>
-        <Cold_Drinks/>
-        <Candies_Gums/>
-        <LinkCategories/>
+  const [isLoader, setLoader] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 1000);
+  });
+
+  if (isLoader) {
+    return <Loader />;
+  } else {
+    return (
+      <div>
+        <Header />
+        <Banner_first />
+        <Banner_Second />
+        <MultipleIteams />
+        <Daily_base />
+        <Rolling_Paper />
+        <Sancks_Munchies />
+        <Hookah />
+        <Mouth_Fresh />
+        <Cold_Drinks />
+        <Candies_Gums />
+        <LinkCategories />
         {/* <Demo_Items/> */}
-        <Footer/>
-    </div>
-  )
+        <Footer />
+      </div>
+    );
+  }
 }
 
-export default Dashboard
+export default Dashboard;
